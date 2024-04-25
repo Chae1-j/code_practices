4f1f5a8008ca94637948025177e4d75873d7cd94
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2024-04-25 21:27:12 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1297.24 ms, Memory: 402 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -7,7 +7,6 @@ class Solution {
 
         for (int i = 0; i < friends.length-1; i++) {
             for (int j = i+1; j < friends.length; j++) {
-
                 String friend1 = friends[i];
                 String friend2 = friends[j];
                 int friend1ToFriend2 = 0;
