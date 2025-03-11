396c489cad5cff55cb3ea94951d5ce61055ddbd2
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2025-03-11 10:52:40 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 16.49 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,24 +1,15 @@
-import java.util.*;
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        ArrayList<String> list = new ArrayList<String>();
-		for(int i = 1; i < food.length; i++) {
-			if(food[i]>1) {
-				food[i] /= 2;
-				for(int j = 1; j <= food[i]; j++) {
-                    
-					list.add(Integer.toString(i));
-				}
-			}
-		}
-		list.add("0");
-		for(int i = list.size()-2; i >= 0; i--) {
-			list.add(list.get(i));
-		}
-		for(int i = 0; i < list.size(); i++) {
-			answer += list.get(i);
-		}
-        return answer;
+        StringBuilder left = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                left.append(i);
+            }
+        }
+
+        return left.toString() + "0" + left.reverse().toString();
+        
     }
 }
